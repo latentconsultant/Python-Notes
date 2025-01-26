a0b839e81314037ffcd8c450f3b930c90a033c59
latentconsultant <92561914+latentconsultant@users.noreply.github.com>
2025-01-26 10:44:30 -0600

    Add files via upload

---
+++ listcomp.py
@@ -0,0 +1,22 @@
+# list comprehensions
+''' Demonstrate basic list comprehensions '''
+# Without list comprehensions:
+odds = []
+for num in range(10):
+    if num % 2:
+        odds.append(num)
+print('Odd numbers:', odds)
+
+# With a list comprehension
+odds = [num for num in range(10) if num % 2]
+print('Odd numbers:', odds)
+
+# More examples
+nums = [str(num) for num in range(10)]
+print('Number strings:', nums)
+
+evens = [num for num in nums if not int(num) % 2]
+print('Even numbers:', evens)
+
+alphabet = [chr(ordinal) for ordinal in range(ord('A'), ord('z') + 1) if chr(ordinal).isalpha()]
+print('Alphabet:', alphabet)
